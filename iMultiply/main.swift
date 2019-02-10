2379f7f3fb9ca86b54149d050ed9cd7dc1579ebf
She <Sheila.Amarga@nike.com>
2019-02-10 10:07:23 -0800

    Initial Commit

---
+++ iMultiply/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  iMultiply
+//
+//  Created by Sheila Amarga on 2/10/19.
+//  Copyright © 2019 Sheila Amarga. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
